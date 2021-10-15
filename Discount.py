1e9e51655b6b5f16c364627142d8fd52e6b5410f
HrideshSingh <75170078+HrideshSingh@users.noreply.github.com>
2021-10-15 09:08:39 +0530

    Add files via upload

---
+++ Discount.py
@@ -0,0 +1,29 @@
+'''
+A newly opened store is offering a 15% discount on the MRP value of any product. 
+
+Write a program that takes the value of a product in rupees as input and outputs the price after discount.
+
+Input Format:
+
+The first line contains a user input number
+
+Output Format:
+
+Print the value after discount as the output
+
+Example:
+
+Input:
+
+100
+
+Output:
+
+85.0
+'''
+a=input()
+b=float(a)
+
+c=b-b*0.15
+
+print(c,end="")
