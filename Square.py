f50034eae4189ecc79cd0eb75ae7ee7985b33034
HrideshSingh <75170078+HrideshSingh@users.noreply.github.com>
2021-10-15 09:10:31 +0530

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,26 @@
+'''
+Write a program to accept a number as input and display the square of the number as the output.
+
+Note: int(input()) can be used to accept user input as an integer in Python.
+
+Input Format:
+
+The first line contains a user input number
+
+Output Format:
+
+Print the square of the number
+
+Example:
+
+Input:
+
+5
+
+Output:
+
+25
+'''
+a=input()
+b=int(a)
+print(b**2,end="")
